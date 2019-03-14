327a105be4e7295a2ba74031b5f389b7a798a869
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-03-14 15:24:12 +0000

    Added isBlessed condition.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,6 +1,8 @@
 fun main() {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
+
 
     if(healthPoints == 100) {
         println(name + " is in excellent health")
@@ -9,7 +11,10 @@ fun main() {
         println(name + " has a few scratches")
 
     }else if(healthPoints >= 75) {
-        println(name + " has some minor wounds")
+
+        if(isBlessed) {
+            println(name + " has some minor wounds but is blessed and healing quickly ")
+        } else { println(name + " has some minor wounds")}
 
     }else if(healthPoints >= 15) {
         println(name + " looks pretty hurt")
