c80de70686374e6187c76a13539cf6f91b410062
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-03-14 16:08:44 +0000

    Added immortality logic

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,7 +2,14 @@ fun main() {
     val name = "Madrigal"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
 
+
+        if(isBlessed && healthPoints > 50 || isImmortal){
+            println("GREEN")
+        } else {
+            println("NONE")
+        }
 
     if(healthPoints == 100) {
         println(name + " is in excellent health")
