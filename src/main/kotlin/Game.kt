9b4c8aaba7b6e141e06fdac675b47e4b03233c96
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-03-15 14:44:50 +0000

    Refactor to use when.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -9,18 +9,15 @@ fun main() {
     val auraColour = if (!auraVisible) "NONE" else "GREEN"
     println(auraColour)
 
-    val healthStatus = if (healthPoints == 100) "is in excellent condition"
-
-    else if(healthPoints in 90 .. 99) " has a few scratches"
-
-    else if (healthPoints in 75 .. 89)
+val healthStatus = when (healthPoints){
+    100 -> "is in excellent condition"
+    in 90 .. 99 -> " has a few scratches"
+    in 75 .. 89 ->
         if(isBlessed) "has some minor wounds but is blessed and healing quickly"
         else  " has some minor wounds"
-
-    else if (healthPoints in 15 .. 74) "is in awful condition"
-
-    else " looks pretty hurt"
-
+    in 15 .. 74 -> "is in awful condition"
+    else ->" looks pretty hurt"
+}
 
     println("$name $healthStatus")
 }
