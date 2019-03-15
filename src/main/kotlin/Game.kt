9703bf644fda598d5842db1f2e7888ffe384d191
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-03-15 14:39:28 +0000

    Refactor to use ranges

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -11,13 +11,13 @@ fun main() {
 
     val healthStatus = if (healthPoints == 100) "is in excellent condition"
 
-    else if(healthPoints >= 90) " has a few scratches"
+    else if(healthPoints in 90 .. 99) " has a few scratches"
 
-    else if (healthPoints < 75)
+    else if (healthPoints in 75 .. 89)
         if(isBlessed) "has some minor wounds but is blessed and healing quickly"
         else  " has some minor wounds"
 
-    else if (healthPoints < 15) "is in awful condition"
+    else if (healthPoints in 15 .. 74) "is in awful condition"
 
     else " looks pretty hurt"
 
